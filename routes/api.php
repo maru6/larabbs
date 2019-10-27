7e8ee33c479ac563483603a8c6626a15a783c478
maru6 <dnakv321@gmail.com>
2019-10-27 15:43:14 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -103,6 +103,9 @@ $api->version('v1', [
             // 发布回复
             $api->post('topics/{topic}/replies', 'RepliesController@store')
                 ->name('api.topics.replies.store');
+            // 管理员回复
+            $api->post('topics/{reply}', 'RepliesController@adminStore')
+                ->name('api.replies.adminreply.store');
             // 删除回复
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
                 ->name('api.topics.replies.destroy');
